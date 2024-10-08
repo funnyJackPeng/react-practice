a0e70d411b43e606b0755cd296c1cbbe57d4aaea
funnyJack <13388883711@163.com>
2024-10-08 12:09:54 +0800

    router 练习

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+
+import {createBrowserRouter} from 'react-router-dom'
+import Main from "../pages/main"
+import Home from "../pages/home"
+const routes = [
+    {
+        path:"/",
+        Component: Main,
+        children:[{
+            path:"home",
+            Component: Home
+        }]
+    }
+]
+
+export default createBrowserRouter(routes)
