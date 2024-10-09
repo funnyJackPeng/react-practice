d532d95ae7926416fd891d00fbe71cae79ebf719
funnyJack <13388883711@163.com>
2024-10-09 11:29:06 +0800

    实现 layout 布局，并将 aside 拆分成组件

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,11 +1,53 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+import CommonAside from '../components/commonAside';
+const { Header, Sider, Content } = Layout;
 
 const Main = () =>{
-  return  <div>
-    main page
-    <Outlet />
-    </div>
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout className='main-container'>
+      <CommonAside />
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
 }
 
 export default Main
