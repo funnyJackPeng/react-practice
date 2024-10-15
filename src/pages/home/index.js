205d20d1759fc51413abc6969bf388739c4ec00e
funnyJack <pengli.he@hkmci.com>
2024-10-15 11:23:11 +0800

    订单统计实现

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { Col, Row, Card, Table } from 'antd';
 import './index.css'
+import * as Icon from "@ant-design/icons";
 import { getData } from '../../api'
 
 const columns = [
@@ -21,6 +22,45 @@ const columns = [
     dataIndex: 'totalBuy'
   }
 ]
+const countData = [
+  {
+    "name": "今日支付订单",
+    "value": 1234,
+    "icon": "CheckCircleOutlined",
+    "color": "#2ec7c9"
+  },
+  {
+    "name": "今日收藏订单",
+    "value": 3421,
+    "icon": "ClockCircleOutlined",
+    "color": "#ffb980"
+  },
+  {
+    "name": "今日未支付订单",
+    "value": 1234,
+    "icon": "CloseCircleOutlined",
+    "color": "#5ab1ef"
+  },
+  {
+    "name": "本月支付订单",
+    "value": 1234,
+    "icon": "CheckCircleOutlined",
+    "color": "#2ec7c9"
+  },
+  {
+    "name": "本月收藏订单",
+    "value": 3421,
+    "icon": "ClockCircleOutlined",
+    "color": "#ffb980"
+  },
+  {
+    "name": "本月未支付订单",
+    "value": 1234,
+    "icon": "CloseCircleOutlined",
+    "color": "#5ab1ef"
+  }
+]
+const generateElement = (name) => React.createElement(Icon[name])
 
 const Home = () => {
   const userImg = require("../../assets/images/user.png")
@@ -56,7 +96,25 @@ const Home = () => {
         </Card>
       </Col>
 
-      <Col span={16}> test2 </Col>
+      <Col span={16}>
+        <div className="num">
+          {
+            countData.map((item, index) => {
+              return (
+                <Card key={index}>
+                    <div className="icon-box" style={{background: item.color}}>
+                      {generateElement(item.icon)}
+                    </div>
+                    <div className="detail">
+                        <p className="num">￥{item.value}</p>
+                        <p className="txt">{item.name}</p>
+                    </div>
+                </Card>
+              )
+            })
+          }
+        </div>
+      </Col>
     </Row>
   );
 }
