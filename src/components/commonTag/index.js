e18793e4f6f31a567def9b0d84f3c6258fa7a256
funnyJackPeng <13388883711@163.com>
2024-10-23 20:50:32 +0800

    实现跳转和删除 tag 的功能

--- src/components/commonTag/index.js
+++ src/components/commonTag/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Space, Tag } from 'antd';
 import './index.css'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { closeTab, setCurrentTab } from '../../store/reducers/tab';
+import { useNavigate } from 'react-router-dom';
 
 const CommonTag = () => {
     const tabList = useSelector(state => state.tab.tabList)
     const currentTab = useSelector(state => state.tab.currentTab)
+    const dispath = useDispatch()
+    const navigate = useNavigate()
     const setTag = (isSelect, item, index) => {
         return (
             isSelect
-                ? <Tag color='#55acee' closeIcon onClose={() => handleClose(item, index)} key={item.name}>{item.label}</Tag>
+                ? <Tag color='#55acee' closeIcon={ tabList.length>1 } onClose={() => handleClose(item, index)} key={item.name}>{item.label}</Tag>
                 : <Tag onClick={() => { handleChange(item) }} key={item.name}>{item.label}</Tag>
         );
     }
     const handleClose = (item, index) => {
-        console.log('关闭了')
+        //调用 closetab 函数
+        dispath(closeTab({...item,index}))
     }
+
     const handleChange = (item) => {
-        console.log('修改了')
+        //切换 current tab
+        dispath(setCurrentTab(item))
     }
+    //每当 currentTab 发生变化时，跳转路由
+    useEffect(()=>{
+        navigate(currentTab.path)
+    },[currentTab])
+
     return (
         <Space className='common-tag'>
             {tabList.map((item, index) => setTag(item.label === currentTab.label, item, index))}
--- src/store/reducers/tab.js
+++ src/store/reducers/tab.js
@@ -25,9 +25,42 @@ const tabSlice =  createSlice({
                if(!state.tabList.some(item=>item.name === val.name)){
                 state.tabList.push(val)
                }
+        },
+        setCurrentTab:(state,{payload:val})=>{
+            state.currentTab = val
+        },
+        closeTab:(state,{payload:val})=>{
+            const tabList = state.tabList
+            const tabListLength = tabList.length
+            const index = val.index
+            /*解决 tabList 被删除干净后读取 currentTab.path undifind 的两种方式 
+            * 1、当删除到最后一个时，填充默认值
+            * 2、list 中只剩一个时，隐藏删除按钮
+            */
+            // if(tabListLength===1){
+            //     state.tabList = [{
+            //         path:'/',
+            //         name:'home',
+            //         label:'首页'
+            //     }]
+            //     state.currentTab={
+            //         path:'/',
+            //         name:'home',
+            //         label:'首页'
+            //     }
+            //     return
+            // }
+
+            if(index === tabListLength-1){
+                state.currentTab = tabList[tabListLength-2]
+            }else{
+                state.currentTab = tabList[index+1]
+            }
+
+            tabList.splice(index,1)
         }
     }
 })
 
-export const { collapseMenu,setTabList } = tabSlice.actions
+export const { collapseMenu,setTabList,setCurrentTab,closeTab } = tabSlice.actions
 export default tabSlice.reducer
